2a3ee775608585525817ff1743a49a83e3e5ddaf
Karthic Kumaran <k3.karthic@protonmail.ch>
2021-05-21 14:46:55 +0000

    format

--- main.tf
+++ main.tf
@@ -23,11 +23,11 @@ provider "oci" {}
 //
 
 resource "oci_objectstorage_bucket" "bucket" {
-    compartment_id = var.compartment
-    name = var.bucket_name
-    namespace = var.bucket_namespace
+  compartment_id = var.compartment
+  name           = var.bucket_name
+  namespace      = var.bucket_namespace
 
-    auto_tiering = "InfrequentAccess"
+  auto_tiering = "InfrequentAccess"
 }
 
 //
@@ -35,33 +35,33 @@ resource "oci_objectstorage_bucket" "bucket" {
 //
 
 resource "oci_identity_user" "s3" {
-    compartment_id = var.compartment
-    name = "ipfs-s3-user"
-    description = "Service account to access ${oci_objectstorage_bucket.bucket.name} using the S3 API"
+  compartment_id = var.compartment
+  name           = "ipfs-s3-user"
+  description    = "Service account to access ${oci_objectstorage_bucket.bucket.name} using the S3 API"
 }
 
 resource "oci_identity_group" "s3" {
-    compartment_id = var.compartment
-    name = "ipfs-s3-group"
-    description = "Group with acess to ${oci_objectstorage_bucket.bucket.name} bucket"
+  compartment_id = var.compartment
+  name           = "ipfs-s3-group"
+  description    = "Group with acess to ${oci_objectstorage_bucket.bucket.name} bucket"
 }
 
 resource "oci_identity_user_group_membership" "s3" {
-    group_id = oci_identity_group.s3.id
-    user_id = oci_identity_user.s3.id
+  group_id = oci_identity_group.s3.id
+  user_id  = oci_identity_user.s3.id
 }
 
 resource "oci_identity_policy" "s3" {
-    compartment_id = var.compartment
-    name = "ipfs-s3-fullaccess"
-    description = "Provide full access to ${oci_objectstorage_bucket.bucket.name} bucket"
+  compartment_id = var.compartment
+  name           = "ipfs-s3-fullaccess"
+  description    = "Provide full access to ${oci_objectstorage_bucket.bucket.name} bucket"
 
-    statements = [
-        "ALLOW GROUP ${oci_identity_group.s3.name} TO MANAGE objects IN TENANCY where target.bucket.name='${oci_objectstorage_bucket.bucket.name}'"
-    ]
+  statements = [
+    "ALLOW GROUP ${oci_identity_group.s3.name} TO MANAGE objects IN TENANCY where target.bucket.name='${oci_objectstorage_bucket.bucket.name}'"
+  ]
 }
 
 resource "oci_identity_customer_secret_key" "s3" {
-    display_name = "ipfs-s3-csk"
-    user_id = oci_identity_user.s3.id
+  display_name = "ipfs-s3-csk"
+  user_id      = oci_identity_user.s3.id
 }
